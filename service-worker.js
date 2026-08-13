f8aa86d5318412da19eca844f5719953ebee52ca
andydoane <andy.doane@npbc.us>
2026-08-13 09:39:45 -0400

    Change PWA BG theme to Black
    Trying to fix iPad top bar problem

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    App scope: /biblozoo/
    ========================================================= */
 
-const CACHE_VERSION = "2026_Aug_12f";
+const CACHE_VERSION = "2026_Aug_12g";
 const SHELL_CACHE =
   `biblozoo-shell-${CACHE_VERSION}`;
 const RUNTIME_CACHE =
