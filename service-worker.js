54bdad2a1ad2c544d35e2abe202c2c1c2193758f
andydoane <andy.doane@npbc.us>
2026-08-31 14:34:27 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    App scope: /biblozoo/
    ========================================================= */
 
-const CACHE_VERSION = "2026_Aug_17_a";
+const CACHE_VERSION = "2026_Aug_31";
 const SHELL_CACHE =
   `biblozoo-shell-${CACHE_VERSION}`;
 const RUNTIME_CACHE =
